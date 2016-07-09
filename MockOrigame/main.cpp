76c22d56d8ca1fab0bd6e8f3dcb8d78d70cb1bc4
clarke <gjzkk2@gmail.com>
2016-07-09 17:32:00 +0800

    Initial Commit

---
+++ MockOrigame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MockOrigame
+//
+//  Created by Clarke on 7/9/16.
+//  Copyright © 2016 PaperWolves. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
